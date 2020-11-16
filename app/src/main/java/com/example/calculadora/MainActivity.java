ef3902972ad24bc8c18de551feb98a79dd6cbdc3
Jandres <darkkhir0@gmail.com>
2020-11-15 21:18:51 -0500

    Suma de dos numeros sencilla

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText num1, num2;
+    private TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se le asignan los elementos en la interfaz a las variables
+        num1 = findViewById(R.id.txtNmbrOne);
+        num2 = findViewById(R.id.txtNmbrTwo);
+        result = findViewById(R.id.lblResult);
+
+    }
+
+    public void calcular (View v){
+        double n1, n2, suma;
+        n1 = Double.parseDouble(num1.getText().toString());
+        n2 = Double.parseDouble((num2.getText().toString()));
+        suma = n1+n2;
+        //Es necesario pasarle el resultado como String
+        result.setText(String.valueOf(suma));
     }
 }
